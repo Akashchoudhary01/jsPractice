f5b5b0d9490cfa0dfd7ccdaf573d52f6b350e0c2
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-11 07:02:10 +0530

    dom

---
+++ js-Questions/Dom.js
@@ -0,0 +1,6 @@
+//what is dom how does it represent the HTML document
+
+// dom stand for  document object model it is a programming interface Represent the html document as structure three  of a object  
+
+let h1 = document.querySelector("h1");
+h1.innerHTML = "Hello World"
