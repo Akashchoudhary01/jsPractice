86cc04ad185277cafe86ccf2c8695344e0aa61ae
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-14 17:57:22 +0530

    click donee

--- eventss/event.js
+++ eventss/event.js
@@ -1,37 +1,50 @@
-let h1 = document.createElement("h1");
-h1.innerText = "Hello World";
-h1.classList.add("hello");
+// let h1 = document.createElement("h1");
+// h1.innerText = "Hello World";
+// h1.classList.add("hello");
 
 
-document.body.appendChild(h1);
+// document.body.appendChild(h1);
 
-let a = document.createElement("a");
-a.innerHTML = "google"
-a.setAttribute("href" , "https://www.google.com");
-document.body.prepend(a);
+// let a = document.createElement("a");
+// a.innerHTML = "google"
+// a.setAttribute("href" , "https://www.google.com");
+// document.body.prepend(a);
 
-let p = document.querySelector("p");
-p.addEventListener("dblclick" , function(){
-   p.style.color = "red"
-})
+// let p = document.querySelector("p");
+// p.addEventListener("dblclick" , function(){
+//    p.style.color = "red"
+// })
 
 
-//input
-let input = document.createElement("input");
-document.body.appendChild(input);
-input.addEventListener("input" , function(inp){
-    if(inp.data !==null){
+// //input
+// let input = document.createElement("input");
+// document.body.appendChild(input);
+// input.addEventListener("input" , function(inp){
+//     if(inp.data !==null){
 
-        console.log(inp.data);
-    }
+//         console.log(inp.data);
+//     }
     
-})
+// })
 
-//change :- change event tab chalta hai jab hum select ya textarea me koi change krte hai 
-let hui = document.getElementById("hui");
-let select = document.querySelector("select");
-select.addEventListener("change" , function(val){
-    console.log(val.target.value);
-    hui.textContent = `${val.target.value} selected `
+// //change :- change event tab chalta hai jab hum select ya textarea me koi change krte hai 
+// let hui = document.getElementById("hui");
+// let select = document.querySelector("select");
+// select.addEventListener("change" , function(val){
+//     console.log(val.target.value);
+//     hui.textContent = `${val.target.value} selected `
+    
+// })
+
+let h1 = document.getElementById("h1");
+
+window.addEventListener("keydown" , function(key){
+    console.log(key.key);
+    if(key.key === " "){
+        h1.textContent = "SPC"
+    }else{
+        h1.textContent = key.key
+
+    }
     
 })
