6292a0f7f004bcd30ab2b2533be59129263d51bd
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-17 06:39:16 +0530

    /

--- neww/thems.js
+++ neww/thems.js
@@ -1,32 +1,29 @@
-function theme(){
-    if(window.matchMedia("(prefers-color-scheme: dark )").match){
-
+function theme() {
+  if (window.matchMedia("(prefers-color-scheme: dark )").match) {
     document.body.classList.add("dark");
     document.body.classList.remove("light");
-}else{
-    
-        document.body.classList.add("light");
-        document.body.classList.remove("dark");
-
-}
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
 }
 
 theme();
 
-let btn = document.querySelector('.toggle');
+let btn = document.querySelector(".toggle");
 
-btn.addEventListener("click" , function(){
-    if(document.body.classList.contains("dark")){
-        document.body.classList.add("light");
-             document.body.classList.remove("dark");
-    }else{
-         document.body.classList.add("dark");
+btn.addEventListener("click", function () {
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  } else {
+    document.body.classList.add("dark");
     document.body.classList.remove("light");
-    }
-})
+  }
+});
 
 window
-    .matchMedia("(prefers-color-scheme: dark )")
-    .addEventListener("change" , function(){
-        theme();
-    })
+  .matchMedia("(prefers-color-scheme: dark )")
+  .addEventListener("change", function () {
+    theme();
+  });
