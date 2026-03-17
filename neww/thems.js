906379946841ea4f6f786c7e2f07c80b0f2652aa
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-17 06:36:41 +0530

    theme toggler

---
+++ neww/thems.js
@@ -0,0 +1,32 @@
+function theme(){
+    if(window.matchMedia("(prefers-color-scheme: dark )").match){
+
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+}else{
+    
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+
+}
+}
+
+theme();
+
+let btn = document.querySelector('.toggle');
+
+btn.addEventListener("click" , function(){
+    if(document.body.classList.contains("dark")){
+        document.body.classList.add("light");
+             document.body.classList.remove("dark");
+    }else{
+         document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    }
+})
+
+window
+    .matchMedia("(prefers-color-scheme: dark )")
+    .addEventListener("change" , function(){
+        theme();
+    })
