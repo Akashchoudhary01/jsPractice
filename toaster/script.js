39426c65d7a80b7f373b4b80491826d07ad315a0
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-18 23:51:38 +0530

    toaster done

--- toaster/script.js
+++ toaster/script.js
@@ -4,27 +4,48 @@ document.body.style.padding = "10px";
 function createToaster(config){
     return function (str){
         let div = document.createElement("div");
-        div.textContent = str;
-        div.className = `inline-block top-5 right-5 ${config.theme === "dark" ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'} px-6 py-3 rounded shadow-lg  pointer-event-none transition-duration: 3s`
 
-        document.querySelector(".parent").appendChild(div)
+        div.textContent = str;
+        div.className = `mb-2 px-6 py-3 rounded shadow-lg pointer-events-none 
+        ${config.theme === "dark" ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`;
+
+        const parent = document.querySelector(".parent");
+
+        // Set position
+        parent.style.position = "fixed";
+
+        parent.className = "parent " +
+            (config.positionX === "right" ? "right-5 " : "left-5 ") +
+            (config.positionY === "bottom" ? "bottom-5 " : "top-5 ");
+
+        parent.appendChild(div);
 
         setTimeout(()=>{  
-            document.querySelector(".parent").removeChild(div)
-        } ,config.duration*1000);
-
+            parent.removeChild(div);
+        }, config.duration * 1000);
     }
-
 }
 
 let toaster = createToaster({
     positionX : "right",
-    positionY : "top",
+    positionY : "bottom",
     theme : "light",
     duration : 3
-})
+});
 
-toaster ("download donee !")
+toaster("Hello :)");
 setTimeout(()=>{
-  toaster("Hello How Are You ")
-} , 2500)
+  toaster("Hello How Are You ");
+}, 3000);
+setTimeout(()=>{
+  toaster("Akash This Side ! ");
+}, 6000);
+setTimeout(()=>{
+  toaster("Hope you are all well and also doing well ! ");
+}, 9000);
+setTimeout(()=>{
+  toaster("Have A greate day ❤️! ");
+}, 12000);
+setTimeout(()=>{
+  toaster("Bye ❤️! ");
+}, 15000);
