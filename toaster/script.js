2f052418a6d324591d733b07f1fcf882e274f10a
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-18 23:27:56 +0530

    /

--- toaster/script.js
+++ toaster/script.js
@@ -1,1 +1,17 @@
 document.body.style.backgroundColor = "gray";
+
+function createToaster(config){
+    return function (){
+
+    }
+
+}
+
+let toaster = createToaster({
+    positionX : "right",
+    positionY : "top",
+    theme : "dark",
+    duration : 3
+})
+
+toaster ("download donee !")
