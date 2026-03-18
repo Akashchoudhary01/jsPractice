ae2830e08e75c1fb2270ad3d4e0533cb02c03519
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-18 23:39:48 +0530

    /

--- toaster/script.js
+++ toaster/script.js
@@ -1,7 +1,17 @@
 document.body.style.backgroundColor = "gray";
+document.body.style.padding = "10px";
 
 function createToaster(config){
-    return function (){
+    return function (str){
+        let div = document.createElement("div");
+        div.textContent = str;
+        div.className = `inline-block top-5 right-5 ${config.theme === "dark" ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'} px-6 py-3 rounded shadow-lg  pointer-event-none transition-duration: 3s`
+
+        document.querySelector(".parent").appendChild(div)
+
+        setTimeout(()=>{  
+            document.querySelector(".parent").removeChild(div)
+        } ,config.duration*1000);
 
     }
 
@@ -10,8 +20,11 @@ function createToaster(config){
 let toaster = createToaster({
     positionX : "right",
     positionY : "top",
-    theme : "dark",
+    theme : "light",
     duration : 3
 })
 
 toaster ("download donee !")
+setTimeout(()=>{
+  toaster("Hello How Are You ")
+} , 2500)
