cfcfa53a3d4e870dd7ca9ecfa4da82c442e5aa45
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-21 18:28:09 +0530

    extends and super keywords done

--- oops/oops.js
+++ oops/oops.js
@@ -1,18 +1,89 @@
-function createCar(name , model , year , color , speed ){
-    this.name = name;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    this.speed = speed;
-    this.slogan = function(text){
+// function createCar(name , model , year , color , speed ){
+//     this.name = name;
+//     this.model = model;
+//     this.year = year;
+//     this.color = color;
+//     this.speed = speed;
+//     this.slogan = function(text){
+//         let h1 = document.createElement("h1");
+//         h1.textContent = text;
+//         h1.style.color = color;
+//         document.body.appendChild(h1);
+//     }
+// }
+
+// let car1 = new createCar("Fortuner" , "legender 4 X 4" , 2026 , "red" , "180");
+// let text = car1.slogan("The fortuner is headturner");
+
+// console.log(car1);
+
+
+// class
+/**
+ * class CreatePencil{
+    constructor(name , price  , company , color){
+        this.name = name;
+        this.price = price;
+        this.color = color;
+        this.company = company;
+     
+    }
+
+    write(text){
         let h1 = document.createElement("h1");
-        h1.textContent = text;
-        h1.style.color = color;
+        h1.textContent = text
+        h1.style.color = this.color;
         document.body.appendChild(h1);
+    }
+
+    erase(){
+        document.body.querySelectorAll("h1").forEach((elem)=>{
+            if(elem.style.color === this.color){
+                elem.remove();
+            }
+        })
     }
 }
 
-let car1 = new createCar("Fortuner" , "legender 4 X 4" , 2026 , "red" , "180");
-let text = car1.slogan("The fortuner is headturner");
+let p1 = new CreatePencil("Natraj" , 10  , "natraj" , "blue");
+let p2 = new CreatePencil("Apsara" , 15  , "Apsara" , "black");
+ */
 
-console.log(car1);
+///
+// extends & super keyword
+
+class User{
+    constructor(name , city , age , email){
+        this.name = name;
+        this.city = city;
+        this.age = age;
+        this.email = email;
+        this.role = "user"
+    }
+
+    checkRole(){
+        return `you are ${this.role}`
+    }
+    write(text){
+        let h1 = document.createElement("h1");
+        h1.textContent = text
+    }
+}
+
+class Admin extends User{
+        constructor(name , city , age , email){
+            super (name , city , age , email)
+            this.role = "ADMIN";
+        }
+
+        remove(){
+            document.querySelectorAll("h1").forEach(function(elem){
+                elem.remove();
+
+            })
+        }
+}
+
+let u1 = new User("Akash" , "Ranchi" , 22 , "akashkrchoudhary007@gmail.com");
+let u2 = new User("Juhi" , "Ranchi" , 19 , "juhiakashchoudhary@gmail.com");
+let Ai = new Admin("luck" , "earth" , 69 , "s@s.s");
