382b3ef20e7f8cfafbe0dd3af9771b1d0a558011
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-22 21:57:29 +0530

    done

--- fetchApi/script.js
+++ fetchApi/script.js
@@ -1,41 +1,49 @@
-fetch("https://randomuser.me/api/?results=3")
-  .then((row) => row.json())
-  .then((data) =>
-    data.results.forEach((user) => {
-        console.log(user);
-        
-      const card = document.createElement("div");
-      card.className = "bg-white rounded-2xl shadow-lg p-6 w-80 text-center";
+function fetchUser() {
 
-      // Image
-      const img = document.createElement("img");
-      img.src = user.picture.medium;
-      img.alt = "User Image";
-      img.className =
-        "w-24 h-24 rounded-full mx-auto mb-4 border-4 border-gray-200";
+    fetch("https://randomuser.me/api/?results=3")
+        .then((res) => res.json())
+        .then((data) => {
 
-      // Name
-      const name = document.createElement("h2");
-      name.className = "text-xl font-semibold text-gray-800";
-      name.textContent = user.name.first +" "+ user.name.last;
+            const container = document.querySelector(".users");
+            container.innerHTML = ""; // clear ONCE
 
-      // Role
-      const email = document.createElement("p");
-      email.className = "text-gray-500 text-sm";
-      email.textContent = user.email;
+            data.results.forEach(function (user) {
 
+                const card = document.createElement("div");
+                card.className = "bg-white rounded-2xl shadow-lg p-6 w-80 text-center";
 
-      // Append all elements
-      card.appendChild(img);
-      card.appendChild(name);
-      card.appendChild(email);
-    
+                // Image
+                const img = document.createElement("img");
+                img.src = user.picture.medium;
+                img.alt = "User Image";
+                img.className =
+                    "w-24 h-24 rounded-full mx-auto mb-4 border-4 border-gray-200";
 
-      // Add to body
-      document.querySelector('.users').appendChild(card);
-    }),
-  );
-// .catch((err)=>{
-//     console.log(err);
+                // Name
+                const name = document.createElement("h2");
+                name.className = "text-xl font-semibold text-gray-800";
+                name.textContent = user.name.first + " " + user.name.last;
 
-// })
+                // Email
+                const email = document.createElement("p");
+                email.className = "text-gray-500 text-sm";
+                email.textContent = user.email;
+
+                // Append
+                card.appendChild(img);
+                card.appendChild(name);
+                card.appendChild(email);
+
+                container.appendChild(card);
+            });
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+        });
+}
+
+// Initial call
+fetchUser();
+
+// Button click
+document.querySelector("#btn").addEventListener("click", fetchUser);
