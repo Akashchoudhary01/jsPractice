481ee5a4897c3b541b2224b595c22b03c015dcee
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-03-26 00:17:22 +0530

    /

--- debounse/script.js
+++ debounse/script.js
@@ -1,6 +1,17 @@
 let input = document.createElement("input");
 document.body.appendChild(input);
 
+function debounse(fnc , delay){
+    let timer;
+    return function (...args){
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
+        fnc(args)
+        } , delay)
+    }
+
+}
+
 input.addEventListener("input" , function(){
     console.log("Hello");
     
